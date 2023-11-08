780c6f0ed760fe17c59323327194eaa0fc655f82
a.golovanov <a.golovanov@goodt.me>
2023-11-08 14:10:30 -0800

    init project

---
+++ cloud-storage/src/test/java/com/example/cloudstorage/CloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
